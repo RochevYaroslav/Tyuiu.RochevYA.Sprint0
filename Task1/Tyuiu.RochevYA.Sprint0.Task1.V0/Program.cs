f76a772466334d9ac1405f42de6e24792f26b445
RochevYaroslav <justmisterstone@gmail.com>
2025-09-10 10:26:24 +0500

    Task1

--- Task1/Tyuiu.RochevYA.Sprint0.Task1.V0/Program.cs
+++ Task1/Tyuiu.RochevYA.Sprint0.Task1.V0/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        int age = 20;
+        int age = 18;
         string name = "Yaroslav";
         Console.WriteLine($"Hello {name} your age is {age}");
     }
