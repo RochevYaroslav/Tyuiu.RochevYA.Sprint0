ac756da5b9d217725cb7cb601c82047c6af61116
RochevYaroslav <justmisterstone@gmail.com>
2025-09-10 23:25:00 +0500

    Task4

---
+++ Task4/Tyuiu.RochevYA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.RochevYA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.RochevYA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(10, DataService.Substraction(15, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(25, DataService.Multiplication(5, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(5, DataService.Division(25, 5));
+        }
+    }
+}
---
+++ Task4/Tyuiu.RochevYA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.RochevYA.Sprint0.Task4.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addition(5, 5));
+        Console.WriteLine(DataService.Substraction(15, 5));
+        Console.WriteLine(DataService.Multiplication(5, 5));
+        Console.WriteLine(DataService.Division(5, 5));
+    }
+}
