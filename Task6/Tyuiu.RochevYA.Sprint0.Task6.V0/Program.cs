3621ad96a834fa429d5fae178018d01dae8ac569
RochevYaroslav <justmisterstone@gmail.com>
2025-09-11 20:30:03 +0500

    Task6

---
+++ Task6/Tyuiu.RochevYA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.RochevYA.Sprint0.Task6.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numsArray = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(DataService.AdditionArray(numsArray));
+        Console.WriteLine(DataService.SubstractionArray(numsArray));
+        Console.WriteLine(DataService.MultiplicationArray(numsArray));
+        Console.ReadKey();
+    }
+}
